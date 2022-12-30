0f52a8faa7f4b6b6d5c68c67884dd908e1eb4240
DanvanthramKK <121683037+DanvanthramKK@users.noreply.github.com>
2022-12-30 22:03:04 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+
+st.set_page_config(page_title="About TECH SRUJANOTSAV",page_icon=":zap:",layout="wide")
+
+st.subheader("Welcome To TECH SRUJANOTSAV  :wave:")
+st.title("Inspiring creativity and curiosity through computing.")
+st.write("Discovering the power of technology!!")
+st.write("---")
+st.subheader("About The Event 🛈")
+st.write("Join us for a day of discovery and innovation at the TECH SRUJANOTSAV Computer Science Expo!")
+st.write("This annual event brings together researchers, and professionals in the field of computer science to share their knowledge and showcase the latest advancements in technology. ")
+st.write("Attendees can explore interactive exhibits, participate in hands-on workshops, and attend talks and panels featuring some of the industry's most influential figures. Whether you're a student, researcher, or professional, the TECH SRUJANOTSAV Computer Science Expo has something for everyone. ")
+st.write("Don't miss this exciting opportunity to learn, connect, and explore the world of computer science!")
+st.write("---")
+st.subheader("Our Creators :v:")
+st.write("Chinmaya Computing Club")
+
+
+
+
+
